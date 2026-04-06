ce436fc181359a7f35ae9b9f54f3dc038b58fd32
abitidagi626-ux <abitidagi626@gmail.com>
2026-04-06 13:25:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const { Telegraf, Markup } = require('telegraf');
 
-// የመጣኸው Token
+// ያንተ ቦት Token
 const bot = new Telegraf('8684712579:AAE9JK0cdSK-cVeycF7xAd_KSrUUqmN5HWI');
 
 // 1. ቦቱ ሲጀመር (Start)
 bot.start((ctx) => {
-    const welcomeText = `እንኳን ወደ Ardi Bingo በሰላም መጡ!\n\nተወዳጅ የቢንጎ ጨዋታ በቤትዎ ሆነው ይጫወቱ። በየ 90 ሰከንዱ በሚደረጉ ጨዋታዎች እድልዎን ይሞክሩና በሽዎች የሚቆጠሩ ብሮችን ያሸንፉ።\n\nለማረጋገጥ ከታች ያለውን "📲 ስልክ ቁጥርዎን ያጋሩ" የሚለውን ይጫኑ።`;
+    const welcomeText = `እንኳን ወደ Ardi Bingo በሰላም መጡ!\n\nተወዳጅ የቢንጎ ጨዋታ በቤትዎ ሆነው ይጫወቱ። በየ 90 ሰከንዱ በሚደረጉ ጨዋታዎች እድልዎን ይሞክሩና በሽዎች የሚቆጠሩ ብሮችን ያሸንፉ።\n\nለመቀጠል እባክዎ ከታች ያለውን "📲 ስልክ ቁጥርዎን ያጋሩ" የሚለውን ይጫኑ።`;
     
     ctx.reply(welcomeText, 
         Markup.keyboard([
@@ -19,14 +19,14 @@ bot.start((ctx) => {
 bot.on('contact', (ctx) => {
     const firstName = ctx.from.first_name;
     
-    // ስልኩ ሲላክ የሚመጣ መልዕክት
     ctx.reply(`✅ በትክክል ተመዝግበዋል ${firstName}!`, Markup.removeKeyboard());
 
     // ከ1 ሰከንድ በኋላ ዋናውን ሜኑ ያመጣል
     setTimeout(() => {
         ctx.reply(`🕹 Welcome To Ardi Bingo!\n\nEvery Square Counts – Grab Your Cartela, Join the Game, and Let the Fun Begin!`, 
             Markup.inlineKeyboard([
-                [Markup.button.webApp('🎮 Play Now', 'https://your-bingo-app.vercel.app')], // እዚህ ጋር የዌብ አፑ ሊንክ ይገባል
+                // ያንተን አዲሱን ሊንክ እዚህ ጋር አስገብቼዋለሁ
+                [Markup.button.webApp('🎮 Play Now', 'https://abitidagi626-ux.github.io/ardi-bingo-real/')], 
                 [Markup.button.callback('💰 Check Balance', 'bal'), Markup.button.callback('💵 Make a Deposit', 'dep')],
                 [Markup.button.callback('📞 Support', 'sup'), Markup.button.callback('📖 Instructions', 'inst')],
                 [Markup.button.callback('📩 Invite', 'inv'), Markup.button.callback('🏆 Win Patterns', 'patt')],
@@ -36,11 +36,9 @@ bot.on('contact', (ctx) => {
     }, 1000);
 });
 
-// ቦቱን ማስነሳት
 bot.launch().then(() => {
-    console.log("Ardi Bingo Bot is running correctly!");
+    console.log("Ardi Bingo Bot አሁን ካንተ GitHub ሊንክ ጋር ተገናኝቷል!");
 });
 
-// ስህተት እንዳይፈጠር
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
